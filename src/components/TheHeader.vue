c84c14b07d82e9f4da49f3468796039c7fc2db37
xyyojl <m13631897256@163.com>
2021-03-27 21:29:28 +0800

    完善项目目录结构&修改代码，运行项目

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,14 @@
+<template>
+    <header>
+      头部组件
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
---
+++ src/views/Recommend.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        推荐组件内容，待完善
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>

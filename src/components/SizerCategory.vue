0375d4ccdec0824e66eb96dfa2735a58f6d96a02
xyyojl <m13631897256@163.com>
2021-04-10 16:17:38 +0800

    SizerCategory 组件实现

---
+++ src/components/SizerCategory.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="sizer-category">
+    <div class="category-list">
+        <span
+            v-for="item in categoryList"
+            :key="item.id"
+            class="item"
+            :class="{'active': selectedKeys.includes(item.id)}"
+            @click="selectItem(item.id)"
+        >
+            {{item.name}}
+        </span>
+    </div>
+    <button class="confirm-btn" @click="confirm">完成</button>
+  </div>
+</template>
+
+<script>
+export default {
+  model: {
+    prop: 'categories',
+    event: 'change'
+  },
+  props: {
+    categories: {
+      type: Array,
+      required: true
+    }
+  },
+  data () {
+    return {
+      categoryList: [],
+      selectedKeys: []
+    }
+  },
+  created () {
+    this.getCategories()
+  },
+  methods: {
+    getCategories () {
+      this.$axios.get('/api/category')
+        .then(res => {
+          const result = res.data.data
+          this.categoryList = result
+        })
+    },
+    confirm () {
+      this.$emit('change', this.selectedKeys)
+    },
+    selectItem (id) {
+      const arr = this.selectedKeys.slice()
+      const index = arr.indexOf(id)
+      if (index > -1) {
+        arr.splice(index, 1)
+      } else {
+        arr.push(id)
+      }
+      this.selectedKeys = arr
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.sizer-category
+  padding 10px 25px
+  .category-list
+    layout-flex()
+    .item
+      padding 6px 12px
+      margin 0 7px 10px 0
+      line-height 1;
+      border-radius $border-radius-base
+      font-size $font-size-small
+      color $color-text-regular
+      border $bordered
+      &.active
+        border-color $color-golden
+        background $color-golden
+        color $color-white
+  .confirm-btn
+    padding 6px 17px
+    background $color-blue
+    color $color-white
+    border none
+    outline none
+    border-radius $border-radius-small
+</style>
--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,9 +1,26 @@
 <template>
   <div class="category">
     <div class="nav-wrapper">
-        <div class="tabs">分类 Tab</div>
-        <div class="tab-content"></div>
-        <div class="mask"></div>
+        <div class="sizer-tabs">
+          <div
+            v-for="(item, index) in cats"
+            :key="index"
+            class="sizer-tab-item"
+            :class="getTabClass(index)"
+            @click="switchTab(index)"
+          >
+            <span class="text">{{ item }}</span>
+            <i class="iconfont icon-drop-down" :class="{'icon-drop-up': index === activeTabIdx}"></i>
+          </div>
+        </div>
+        <div class="sizer-tab-content" v-show="activeTabIdx !== -1">
+          <SizerCategory
+            v-show="activeTabIdx === 0"
+            v-model="params.categories"
+            @change="getMovies"
+          />
+        </div>
+        <div class="sizer-mask" v-show="activeTabIdx !== -1" @click="closeTab"></div>
     </div>
     <div class="movie-list" v-show="!loading">
       <Scroll v-show="movieList.length" :data="movieList">
@@ -15,9 +32,14 @@
 </template>
 
 <script>
+import SizerCategory from '@/components/SizerCategory'
 export default {
+  components: {
+    SizerCategory
+  },
   data () {
     return {
+      cats: ['分类', '已上映', '评分'],
       loading: true,
       movieList: [],
       params: {
@@ -27,7 +49,8 @@ export default {
         status: '1',
         // 评分
         rate: [0, 10]
-      }
+      },
+      activeTabIdx: -1
     }
   },
   created () {
@@ -35,6 +58,7 @@ export default {
   },
   methods: {
     getMovies () {
+      this.activeTabIdx = -1
       const { categories, rate, status } = this.params
       const params = {
         categories: !categories.length ? undefined : JSON.stringify(categories),
@@ -47,12 +71,67 @@ export default {
           this.movieList = result
           this.loading = false
         })
+    },
+    getTabClass (index) {
+      return {
+        active: index === this.activeTabIdx,
+        disable: index === 2 && this.params.status === '0'
+      }
+    },
+    switchTab (index) {
+      // 点击相同相当于关闭
+      if (index === this.activeTabIdx) {
+        this.activeTabIdx = -1
+        return
+      }
+
+      // 当选择未上映的时候，评分不可选
+      if (this.params.status === '0' && index === 2) return
+
+      this.activeTabIdx = index
+      // TODO：额外做处理
+    },
+    closeTab () {
+      this.activeTabIdx = -1
     }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-.movie-list
+.sizer-tabs
+  layout-flex()
+  height 40px
+  line-height 40px
+  border-bottom $bordered
+  .sizer-tab-item
+    position relative
+    flex 1
+    text-align center
+    font-size $font-size-small
+    color $color-text-regular
+    &.active
+      color $color-text-primary
+    &.disable
+      color $color-text-lighter
+    &:not(:first-child)::before
+      position absolute
+      content: ''
+      display block
+      height 20px
+      top 10px
+      left 0
+      border-left 1px solid #e8e8e8
+.sizer-tab-content
+  position absolute
+  width 100%
+  top 41px
+  z-index 20
+  background $color-white
+.sizer-mask
   page-fixed(97px)
+  z-index 15
+  background rgba(0, 0, 0, 0.5)
+.movie-list
+  page-fixed(97px);
 </style>
